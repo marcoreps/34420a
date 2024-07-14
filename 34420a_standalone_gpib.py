9441242ffb84dd37ac063004e70d8f5862104cef
reps <marco@reps.cc>
2024-07-14 19:37:19 +0200

    asdf

---
+++ 34420a_standalone_gpib.py
@@ -0,0 +1,39 @@
+import pyvisa,time,csv
+from datetime import datetime
+from pyvisa.constants import StopBits, Parity
+rm = pyvisa.ResourceManager()
+my_instrument = rm.open_resource('GPIB0::8::INSTR')
+my_instrument.timeout = 20000
+my_instrument.write('*RST')
+my_instrument.write('*CLS')
+#my_instrument.write('SYSTem:REMote')
+time.sleep(3)
+print(my_instrument.query('*IDN?'))
+#print(my_instrument.query('SYSTem:ERRor?'))
+my_instrument.write("DISPlay OFF")
+
+my_instrument.write("CONFigure:VOLTage:DC 0.001, MAX, (@FRONt1)")
+my_instrument.write("ROUTe:TERMinals FRONt1")
+my_instrument.write("SENSe:VOLTage:DC:NPLCycles 5")
+my_instrument.write("INPut:FILTer:STATe OFF")
+my_instrument.write("TRIGger:DELay 0")
+my_instrument.write("OUTPut:STATe OFF")
+
+my_instrument.write("SENSe:NULL ONCE")
+
+my_instrument.write("TRIGger:SOURce IMMediate")
+
+
+
+timestr = time.strftime("%Y%m%d-%H%M%S_")
+with open('csv/'+timestr+'HP_34420A_short_NPLC5.csv', mode='w') as csv_file:
+    fieldnames = ['time', '34420a_volt']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    clock=datetime.now()
+    while True:
+        val = float(my_instrument.query("READ?"))
+        writer.writerow({'time':time.time(), '34420a_volt': val})
+        #print(val)
+        print( "Effective reading output rate: "+str(1/(datetime.now()-clock).total_seconds()))
+        clock=datetime.now()
